5da7d17fc3f688f3c4b68e8208dcf7480772fbb1
gui <111365817+guilhermessj@users.noreply.github.com>
2023-07-28 10:57:58 -0300

    js added

---
+++ Scripts/script.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".collapse-from-right")
+const btnToggle = document.querySelector("#button-modified")
+
+btnToggle.addEventListener.forEach(n =>
+    n.addEventListener('click', () =>{
+        navbar.classList.remove("show")
+        btnToggle.classList.remove("show")
+    })
+    )
